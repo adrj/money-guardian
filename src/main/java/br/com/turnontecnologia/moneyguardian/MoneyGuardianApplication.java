4ea4c20b5d39372a7ffe7917e88b6a5c3f1c3053
Adalto dos Reis Junior <adalto.junior@gmail.com>
2024-02-02 16:19:57 -0300

    start the project

---
+++ src/main/java/br/com/turnontecnologia/moneyguardian/MoneyGuardianApplication.java
@@ -0,0 +1,13 @@
+package br.com.turnontecnologia.moneyguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyGuardianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/turnontecnologia/moneyguardian/MoneyGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.turnontecnologia.moneyguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
